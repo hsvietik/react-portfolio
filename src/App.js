a97d595008845f03a07e8ffb7b03d0cb997bc22f
Hanna Svietik <hsvietik@gmail.com>
2023-06-09 14:16:36 +0100

    hashrouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
